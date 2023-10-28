1ed3879164157ee5e9ad114c20a9f56c4dc17972
Cazim99 <34762237+Cazim99@users.noreply.github.com>
2023-10-28 07:20:20 +0200

    Update test.py

--- ConfigLoader/test.py
+++ ConfigLoader/test.py
@@ -1,2 +1,18 @@
 from ConfigLoader import ConfigLoader
 
+
+config_file_informations = {
+    "settings": # SECTION NAME
+        {
+            'items':{ # SECTION ITEMS 
+                'full_screen':'bool',
+                'dev_mode':'bool',
+                'screen_size':'other',
+                'server_host':'str',
+                'server_port':'int',
+            }
+        },
+}
+
+CONFIGURATIONS = ConfigLoader.Load("config.ini", config_file_informations)
+print(CONFIGURATIONS)
